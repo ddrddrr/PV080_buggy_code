94824b0a03e11b447b8a2073866d2f831bd0ad84
ddrddrr <asanikor@bk.ru>
2023-03-29 08:54:42 +0200

    bbb2

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -37,6 +37,7 @@ def load_yaml(filename):
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
